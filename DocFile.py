2cc513e9729382baf84474f67ffd9c2ef1275a81
armiro <arman.hgh.1374@gmail.com>
2018-07-15 09:58:24 +0430

    pdf/word files handler

--- DocFile.py
+++ DocFile.py
@@ -13,15 +13,6 @@ PARA = WORD_NAMESPACE + 'p'
 TEXT = WORD_NAMESPACE + 't'
 
 
-def is_english(complex_text):
-    try:
-        complex_text.encode(encoding='utf-8').decode('ascii')
-    except UnicodeDecodeError:
-        return False
-    else:
-        return True
-
-
 class DocFile:
 
     def __init__(self, name):
@@ -62,26 +53,5 @@ class DocFile:
         # if text:
         #     paragraphs.append(''.join(text))
         return paragraphs
-
-
-# two = DocFile('test.docx')
-# two.word2text()
-one = DocFile('test.pdf')
-txt = one.pdf2text()
-txt.encode('utf-8')
-txt = txt.strip().split()
-txt = txt[::-1]
-
-# print(txt)
-
-cleared = list()
-for word in txt:
-    if is_english(word):
-        pass
-    else:
-        word = word[::-1]
-    cleared.append(word)
-
-print(cleared)
 
 
