5f6ac92ad506ff9cf3e9f8a1e67153ffdbccf36d
armiro <arman.hgh.1374@gmail.com>
2018-07-16 12:05:01 +0430

    extracting each 2-lines from text

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,13 +1,36 @@
 from DocFileObjects import *
 
 
-def line_extractor(text):
-    for text in raw_text:
-        print(text)
-        text_lines = raw_text.split("\n")
+def line_extractor(input_text):
+    text_lines = list()
+    for page_text in input_text:
+        text_lines.append(page_text.split("\n"))
     return text_lines
+
+
+def line_coupler(text_lines):
+    line_couples = list()
+    for page_text in text_lines:
+        for line_idx in range(0, len(page_text)-1, 2):
+            # print(line_idx)
+            line_couples.append([page_text[line_idx], page_text[line_idx + 1]])
+    return line_couples
+
+
+def font_extractor(input_text):
+    pass
+
+
+def blocking_feature_extractor(line_couples):
+    for line_couple in line_couples:
+        pass
 
 
 lines = line_extractor(raw_text)
 # print(raw_text)
-print(lines)
+# print(lines)
+couples = line_coupler(lines)
+# print(couples)
+
+
+
