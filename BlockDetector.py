66a889da55da6cfab79281813bdc735d7154fef3
Bahaareh <bn_1377@yahoo.com>
2018-07-19 14:59:49 +0430

    modified svm

--- BlockDetector.py
+++ BlockDetector.py
@@ -1,23 +1,39 @@
 import json
 from sklearn import svm
 
-with open('blocks/Blocks - CV%.json', 'r') as file:
-    data = json.load(file)
-
+features_train = []
+labels_train = []
+for i in (4, 5, 6, 7, 9, 10, 12, 13, 14):
+    path = 'blocks/Blocks - CV%d.json' % i
+    with open(path, 'r') as f:
+        data = json.load(f)
+    for elements in data:
+        features_train.append(
+            [elements['font_change'], elements['size_change'], elements['has_dot'], elements['has_keyword']])
+        labels_train.append(elements['label'])
 # content = []
 # for i in data:
 #     for j in i['content'][0]:
 #         content.append(j[0])
 # print(content)
-features = []
-labels = []
-for elements in data:
-    features.append([elements['font_change'], elements['size_change'], elements['has_dot'], elements['has_keyword']])
-    labels.append(elements['label'])
-# print(features)
-# print(labels)
+
+features_test = []
+labels_test = []
+with open('./Blocks.json', 'r') as test:
+    data_test = json.load(test)
+
+for e in data_test:
+    features_test.append(
+        [e['font_change'], e['size_change'], e['has_dot'], e['has_keyword']])
+
+# print(features_train)
+# print(labels_train)
+print(features_test)
 
 clf = svm.SVC(kernel='rbf')
-clf.fit(features, labels)
-print(clf.predict([[0, 0, 0, 1]]))
+clf.fit(features_train, labels_train)
 
+for f in features_test:
+    labels_test.append(clf.predict([f]))
+
+print(labels_test)
